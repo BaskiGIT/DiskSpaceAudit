711e85ac4f8a20885eeb95496243be66581265fb
Baskar Lingam <bskr.psgtech@gmail.com>
2016-09-19 18:53:05 +0200

    This works. But need to fine tune more.

--- DiskSpaceAudit.ps1
+++ DiskSpaceAudit.ps1
@@ -1,30 +1,47 @@
-$Drive = get-wmiobject win32_logicaldisk -Filter "DriveType='3'" -ComputerName FQDN_of_server | Select-Object Size, FreeSpace, DeviceID
-# $Drive
-# $Drive[0].FreeSpace
+##############
+
+# Author        : Baskar Lingam Ramachandran (B.Ramachandran@iaea.org)
+# Created Date  : 19th Sept 2016
+
+##############
+
+function Send-Mail
+{
+    Param ([string]$serverName, [string]$DriveName, [string]$FreeSpace)
+    $SMTPServer = "blahblahblah"
+    $SMTPPort = "i_cant_tell"
+    $from = "DoesNotMatter"
+    $to = "DoYouWantToKnow"
+    $Subject = "ATTENTION NEEDED: Disk Space issue in $serverName"
+    $body = "I, DISK $DriveName in $serverName have only $FreeSpace GB free space. Please clean me up."
+    Send-MailMessage -From $from -To $to -Subject $Subject -Body $body -SmtpServer $SMTPServer
+}
+
+function Send-MailNoAction
+{
+    Param ([string]$serverName, [string]$DriveName, [string]$FreeSpace)
+    $SMTPServer = "blahblahblah"
+    $SMTPPort = "i_cant_tell"
+    $from = "DoesNotMatter"
+    $to = "DoYouWantToKnow"
+    $Subject = "ATTENTION not NEEDED: Disk Space usage in $serverName"
+    $body = "I, DISK $DriveName in $serverName have $FreeSpace GB of enough free space. Relax."
+    Send-MailMessage -From $from -To $to -Subject $Subject -Body $body -SmtpServer $SMTPServer
+}
+
+$server = $env:computername
+$Drive = get-wmiobject win32_logicaldisk -Filter "DriveType='3'" | Select-Object Size, FreeSpace, DeviceID
 $count = $Drive.Count
-$count
 for($i = 0; $i -lt $count; $i++)
 {
     $freespace = $Drive[$i].FreeSpace/1GB
     $id = $Drive[$i].DeviceId
-    if($freespace -gt 10)
+    if($freespace -lt 10)
     {
-        Write-Host "No alert needed for $id"
+        Send-Mail -serverName $server -DriveName $id -FreeSpace $freespace
     }
     else
     {
-        Write-Host "Alert needed for $id"
+        Send-MailNoAction -serverName $server -DriveName $id -FreeSpace $freespace
     }
-}
-
-function Send_Mail
-{
-    Param ([string]$serverName, [string]$DriveName, [string]$FreeSpace)
-    $SMTPServer = "blahblahblah"
-    $SMTPPort = "i-cant_tell"
-    $from = "do_you_need_it"
-    $to = "you_know_it"
-    $Subject = "ATTENTION NEEDED: Disk Space issue in $serverName"
-    $body = "DISK $DriveName in $serverName has $FreeSpace GB free space only"
-    Send-MailMessage -From $from -To $to -Subject $Subject -Body $body -SmtpServer $SMTPServer
 }
